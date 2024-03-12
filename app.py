fe304bf4ac4de39a438a5161e52a5182054c5d64
Judy-jebet <160907377+Judy-jebet@users.noreply.github.com>
2024-03-12 06:15:47 +0000

    SUPERMARKET SALES

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def predict():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=9080, debug=True)
+    app.run(host="0.0.0.0", port=9090, debug=True)
